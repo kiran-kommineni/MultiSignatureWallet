d96c8ef4573c4e5f61bcbf74fd21722c1ed524a2
kiran kommineni <kirankommineni@hotmail.com>
2018-07-23 19:10:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import MultiSigWallet from './Components/MultiSigWallet'
+
+import './App.css'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <MultiSigWallet />
+      </div>
+    );
+  }
+}
+
+export default App
